857ce2e281957f622165f71f9809f7525b250a32
Alex Adamovic <alexanderadamovic@gmail.com>
2022-03-18 09:42:21 -0700

    add properties to models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+
 namespace Factory.Models
 {
   public class Engineer
   {
-    
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public string EngineerDetails { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
   }
 }
